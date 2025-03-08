8f30ad4f555109226fda5a05b264c6e9113d4648
rstrutt <rich.strutt@gmail.com>
2025-03-08 09:57:04 -0500

    Trying to get icons working

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Text, View, useColorScheme } from 'react-native';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 function EntryScreen() {
     return (
@@ -36,11 +37,31 @@ function App(): React.JSX.Element {
     return (
         <NavigationContainer>
             <Tab.Navigator
-                screenOptions={{
-                    tabBarActiveTintColor: isDarkMode ? Colors.dark : Colors.light,
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ color, size }) => {
+                        let iconName;
+
+                        if (route.name === 'Entry') {
+                            console.log("in Entry");
+                            iconName = 'add-to-list';
+                        } else if (route.name === 'View') {
+                            console.log("in View");
+                            iconName = 'ios-eye';
+                        } else if (route.name === 'Metrics') {
+                            console.log("in Metrics");
+                            iconName = 'ios-stats-chart';
+                        }
+                        else{
+                            console.log("in else");
+                            iconName = 'xx';
+                        }
+
+                        return <Icon name={iconName} size={size} color={color} />;
+                    },
+                    tabBarActiveTintColor: isDarkMode ? Colors.light : Colors.dark,
                     tabBarInactiveTintColor: isDarkMode ? Colors.dark : Colors.light,
                     tabBarStyle: { backgroundColor: isDarkMode ? Colors.darker : Colors.lighter },
-                }}>
+                })}>
                 <Tab.Screen name="Entry" component={EntryScreen} />
                 <Tab.Screen name="View" component={ViewScreen} />
                 <Tab.Screen name="Metrics" component={MetricsScreen} />
