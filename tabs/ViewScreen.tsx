35d332fb1abb855fdb9f606750f0071670797e10
rstrutt <rich.strutt@gmail.com>
2025-03-08 15:40:56 -0500

    Make the display of metrics be in the same order as the file

--- tabs/ViewScreen.tsx
+++ tabs/ViewScreen.tsx
@@ -17,12 +17,13 @@ const ViewScreen = () => {
     const [refreshing, setRefreshing] = useState(false);
     const [selectedMetrics, setSelectedMetrics] = useState<string[]>([]);
     const [allMetrics, setAllMetrics] = useState<string[]>([]);
+    const [loadedMetrics, setLoadedMetrics] = useState<string[]>([]);
     const [open, setOpen] = useState(false);
 
     const loadEntries = async () => {
         // Load the metrics values and the metric names
         const loadedMericValues = await readMetricValuesFromFile();
-        const loadedMetrics = await readMetricsFromFile()
+        const loadedMetrics = await readMetricsFromFile();
         // Store the loaded metric values
         setEntries(loadedMericValues);
         // Build and save a set of metric names to use in the dropdown
@@ -32,6 +33,7 @@ const ViewScreen = () => {
         metrics.sort((a, b) => loadedMetrics.indexOf(a) - loadedMetrics.indexOf(b));
         setAllMetrics(metrics);
         setSelectedMetrics(metrics); // Select all metrics by default
+        setLoadedMetrics(loadedMetrics); // Save the loaded metrics for ordering
     };
 
     useEffect(() => {
@@ -95,7 +97,7 @@ const ViewScreen = () => {
                 placeholder="Select Metrics"
                 containerStyle={{ height: 40 }}
                 style={{ backgroundColor: isDarkMode ? '#333' : '#fff' }}
-                // dropDownStyle={{ backgroundColor: isDarkMode ? '#333' : '#fff' }}
+                dropDownContainerStyle={{ backgroundColor: isDarkMode ? '#333' : '#fff' }}
             />
             <ScrollView
                 contentContainerStyle={{ paddingTop: 16 }}
@@ -103,59 +105,61 @@ const ViewScreen = () => {
                     <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
                 }
             >
-                {Object.keys(groupedEntries).map(metric => (
-                    <View key={metric} style={{ marginBottom: 16 }}>
-                        <Text style={{ fontSize: 18, fontWeight: 'bold', marginBottom: 8 }}>{metric}</Text>
-                        <LineChart
-                            data={{
-                                labels: groupedEntries[metric].map(entry => formatDateTime(entry.dateTime)),
-                                datasets: [
-                                    {
-                                        data: groupedEntries[metric].map(entry => entry.value)
+                {loadedMetrics.map(metric => (
+                    groupedEntries[metric] && (
+                        <View key={metric} style={{ marginBottom: 16 }}>
+                            <Text style={{ fontSize: 18, fontWeight: 'bold', marginBottom: 8 }}>{metric}</Text>
+                            <LineChart
+                                data={{
+                                    labels: groupedEntries[metric].map(entry => formatDateTime(entry.dateTime)),
+                                    datasets: [
+                                        {
+                                            data: groupedEntries[metric].map(entry => entry.value)
+                                        }
+                                    ]
+                                }}
+                                width={Dimensions.get('window').width - 32} // from react-native
+                                height={220}
+                                yLabelsOffset={-10} // Adjust this value to position the label correctly
+                                chartConfig={{
+                                    backgroundColor: '#d3d3d3', // light grey
+                                    backgroundGradientFrom: '#d3d3d3', // light grey
+                                    backgroundGradientTo: '#d3d3d3', // light grey
+                                    decimalPlaces: 2, // optional, defaults to 2dp
+                                    color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`, // black line color
+                                    labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`, // black text color
+                                    style: {
+                                        borderRadius: 16
+                                    },
+                                    propsForDots: {
+                                        r: '3',
                                     }
-                                ]
-                            }}
-                            width={Dimensions.get('window').width - 32} // from react-native
-                            height={220}
-                            yLabelsOffset={-10} // Adjust this value to position the label correctly
-                            chartConfig={{
-                                backgroundColor: '#d3d3d3', // light grey
-                                backgroundGradientFrom: '#d3d3d3', // light grey
-                                backgroundGradientTo: '#d3d3d3', // light grey
-                                decimalPlaces: 2, // optional, defaults to 2dp
-                                color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`, // black line color
-                                labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`, // black text color
-                                style: {
+                                }}
+                                bezier
+                                style={{
+                                    marginVertical: 8,
                                     borderRadius: 16
-                                },
-                                propsForDots: {
-                                    r: '3',
-                                }
-                            }}
-                            bezier
-                            style={{
-                                marginVertical: 8,
-                                borderRadius: 16
-                            }}
-                        />
-                        {groupedEntries[metric].map((entry) => (
-                            <View key={`${entry.dateTime}-${entry.metric}`} style={{ flexDirection: 'row', alignItems: 'center', marginVertical: 8 }}>
-                                <Text style={{ flex: 1 , fontSize: 16}}>{formatDateTime(entry.dateTime)}</Text>
-                                <TextInput
-                                    style={{ borderColor: 'gray', borderWidth: 1, padding: 8, width: 100, borderRadius: 8 }}
-                                    keyboardType="numeric"
-                                    value={entry.value.toString()}
-                                    onChangeText={(value) => handleValueChange(entry.dateTime, entry.metric, value)}
-                                />
-                                <TouchableOpacity onPress={() => handleSave(entry.dateTime, entry.metric, entry.value)} style={{ marginHorizontal: 8 }}>
-                                    <Icon name="save" size={20} color={isDarkMode ? '#888' : '#555'} />
-                                </TouchableOpacity>
-                                <TouchableOpacity onPress={() => handleDelete(entry.dateTime, entry.metric, entry.value)} style={{ marginHorizontal: 8 }}>
-                                    <Icon name="trash" size={20} color={isDarkMode ? '#888' : '#555'} />
-                                </TouchableOpacity>
-                            </View>
-                        ))}
-                    </View>
+                                }}
+                            />
+                            {groupedEntries[metric].map((entry) => (
+                                <View key={`${entry.dateTime}-${entry.metric}`} style={{ flexDirection: 'row', alignItems: 'center', marginVertical: 8 }}>
+                                    <Text style={{ flex: 1 , fontSize: 16}}>{formatDateTime(entry.dateTime)}</Text>
+                                    <TextInput
+                                        style={{ borderColor: 'gray', borderWidth: 1, padding: 8, width: 100, borderRadius: 8 }}
+                                        keyboardType="numeric"
+                                        value={entry.value.toString()}
+                                        onChangeText={(value) => handleValueChange(entry.dateTime, entry.metric, value)}
+                                    />
+                                    <TouchableOpacity onPress={() => handleSave(entry.dateTime, entry.metric, entry.value)} style={{ marginHorizontal: 8 }}>
+                                        <Icon name="save" size={20} color={isDarkMode ? '#888' : '#555'} />
+                                    </TouchableOpacity>
+                                    <TouchableOpacity onPress={() => handleDelete(entry.dateTime, entry.metric, entry.value)} style={{ marginHorizontal: 8 }}>
+                                        <Icon name="trash" size={20} color={isDarkMode ? '#888' : '#555'} />
+                                    </TouchableOpacity>
+                                </View>
+                            ))}
+                        </View>
+                    )
                 ))}
             </ScrollView>
         </View>
