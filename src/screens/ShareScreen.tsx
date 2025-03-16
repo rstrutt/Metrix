4b0875dc0771293786954bc45f32f99694b2934b
rstrutt <rich.strutt@gmail.com>
2025-03-16 13:47:04 -0400

    Add metric definitions to share screen

--- src/screens/ShareScreen.tsx
+++ src/screens/ShareScreen.tsx
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { View, TextInput, TouchableOpacity, Text, ScrollView } from 'react-native';
 import { useColorScheme } from 'react-native';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
-import { readAppFile, writeAppFile, METRIC_VALUES_FILE_PATH } from '../utils/fileUtils.ts';
+import { readAppFile, writeAppFile, METRIC_VALUES_FILE_PATH,  METRICS_FILE_PATH} from '../utils/fileUtils.ts';
 
 const ShareScreen = () => {
     const [fileContent, setFileContent] = useState('');
+    const [definitionsFileContent, setDefinitionsFileContent] = useState('');
     const isDarkMode = useColorScheme() === 'dark';
 
     const loadFileContent = async () => {
@@ -13,28 +14,43 @@ const ShareScreen = () => {
         setFileContent(content);
     };
 
+    const loadDefinitionsFileContent = async () => {
+        const content = await readAppFile(METRICS_FILE_PATH);
+        setDefinitionsFileContent(content);
+    };
+
     useEffect(() => {
         loadFileContent();
+        loadDefinitionsFileContent();
     }, []);
 
     const saveFileContent = async () => {
         await writeAppFile(METRIC_VALUES_FILE_PATH, fileContent);
+        await writeAppFile(METRICS_FILE_PATH, definitionsFileContent);
     };
 
     const handleReload = async () => {
         await loadFileContent();
+        await loadDefinitionsFileContent();
     };
 
     return (
         <View style={{ flex: 1, padding: 16, backgroundColor: isDarkMode ? Colors.darker : Colors.lighter }}>
-            <ScrollView>
+
                 <TextInput
                     value={fileContent}
                     onChangeText={setFileContent}
                     multiline
-                    style={{ borderColor: 'gray', borderWidth: 1, padding: 8, borderRadius: 8, height: 400, textAlignVertical: 'top', color: isDarkMode ? '#fff' : '#000' }}
+                    style={{ borderColor: 'gray', borderWidth: 1, marginVertical: 5, padding: 8, borderRadius: 8, height: 200, textAlignVertical: 'top', color: isDarkMode ? '#fff' : '#000' }}
                 />
-            </ScrollView>
+
+                <TextInput
+                    value={definitionsFileContent}
+                    onChangeText={setDefinitionsFileContent}
+                    multiline
+                    style={{ borderColor: 'gray', borderWidth: 1, marginVertical: 5, padding: 8, borderRadius: 8, height: 200, textAlignVertical: 'top', color: isDarkMode ? '#fff' : '#000' }}
+                />
+
             <TouchableOpacity onPress={handleReload} style={{ backgroundColor: isDarkMode ? '#444' : '#ddd', padding: 12, borderRadius: 8, alignItems: 'center', marginTop: 16 }}>
                 <Text style={{ color: isDarkMode ? '#fff' : '#000', fontSize: 16 }}>Reload</Text>
             </TouchableOpacity>
