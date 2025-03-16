88e51282c656c3a696874a8df32c960f21c914a7
rstrutt <rich.strutt@gmail.com>
2025-03-16 13:47:38 -0400

    Add metric definitions to share screen.  new icons

--- src/utils/fileUtils.ts
+++ src/utils/fileUtils.ts
@@ -52,16 +52,38 @@ export const saveMetricsToFile = async (metricsArray: { name: string, min_thresh
     }
 };
 
-export const readMetricValuesFromFile = async () => {
+export const readMetricValuesFromFile = async (convertFromUTC: boolean) => {
     try {
         const fileExists = await exists(METRIC_VALUES_FILE_PATH);
         if (!fileExists) {
             await writeFile(METRIC_VALUES_FILE_PATH, '', 'utf8');
         }
         const fileContent = await readFile(METRIC_VALUES_FILE_PATH, 'utf8');
+
         return fileContent.split('\n').filter(Boolean).map(line => {
-            const [dateTime, metric, value] = line.split(',');
-            return { dateTime, metric, value: parseFloat(value) };
+            const [dateTimeFromFile, metric, value] = line.split(',');
+
+            // Switch the dateTime from UTC (in the file) to local if we asked for that
+            if (convertFromUTC){
+                let dateTimeUTCString = dateTimeFromFile;
+                if (!dateTimeFromFile.includes('T')) {
+                    // If the dateTime is not in the format 'yyyy-mm-ddThh:mm:ssZ', then convert it
+                    dateTimeUTCString = dateTimeFromFile.replace(' ', 'T') + ':00Z'
+                }
+
+                console.log(dateTimeUTCString);
+                const dateTimeUTC = new Date(dateTimeUTCString);
+                const formattedDate = dateTimeUTC.toLocaleDateString('en-CA'); // 'en-CA' locale ensures 'yyyy-mm-dd' format
+                const formattedTime = dateTimeUTC.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false }); // 'hh:mm' format
+                const dateTimeLocalString = `${formattedDate} ${formattedTime}`;
+                console.log(dateTimeLocalString);
+
+                return { dateTime: dateTimeLocalString, metric, value: parseFloat(value) };
+            }
+            else{
+                return { dateTime: dateTimeFromFile, metric, value: parseFloat(value) };
+            }
+
         });
     } catch (error) {
         console.error('Error reading metric values file:', error);
@@ -74,9 +96,16 @@ export const saveMetricValuesToFile = async (metricValues: { [key: string]: numb
         // Read the existing contents of the file
         const existingFileContent = await readFile(METRIC_VALUES_FILE_PATH, 'utf8');
 
+        // File is always UTC in the file, but the incoming dateTime string is in local.  Need to convert to UTC
+        const dateTimeUTC = new Date(dateTime);
+        // Now pull out the UTC values
+        const dateTimeUTCDateString = dateTimeUTC.toISOString().split('T')[0]
+        const dateTimeUTCTimeString = dateTimeUTC.toISOString().split('T')[1].slice(0, 5)
+        const dateTimeUTCString = `${dateTimeUTCDateString} ${dateTimeUTCTimeString}`;
+
         // Prepare the new content to be appended
         const newFileContent = Object.entries(metricValues)
-            .map(([metric, value]) => `${dateTime},${metric},${value}`)
+            .map(([metric, value]) => `${dateTimeUTCString},${metric},${value}`)
             .join('\n');
 
         // Append the new content to the existing content
@@ -91,10 +120,19 @@ export const saveMetricValuesToFile = async (metricValues: { [key: string]: numb
 
 export const updateMetricValueInFile = async (updatedEntry: { dateTime: string, metric: string, value: number }) => {
     try {
-        const entries = await readMetricValuesFromFile();
+
+        // File is always UTC in the file, but the incoming dateTime string is in local.  Need to convert to UTC
+        const dateTimeUTC = new Date(updatedEntry.dateTime);
+        // Now pull out the UTC values
+        const dateTimeUTCDateString = dateTimeUTC.toISOString().split('T')[0]
+        const dateTimeUTCTimeString = dateTimeUTC.toISOString().split('T')[1].slice(0, 5)
+        const dateTimeUTCString = `${dateTimeUTCDateString} ${dateTimeUTCTimeString}`;
+
+
+        const entries = await readMetricValuesFromFile(false);
         const updatedEntries = entries.map(entry =>
-            entry.dateTime === updatedEntry.dateTime && entry.metric === updatedEntry.metric
-                ? updatedEntry
+            entry.dateTime === dateTimeUTCString && entry.metric === updatedEntry.metric
+                ? {dateTime: dateTimeUTCString, metric: updatedEntry.metric, value: updatedEntry.value}
                 : entry
         );
         const fileContent = updatedEntries.map(entry => `${entry.dateTime},${entry.metric},${entry.value}`).join('\n');
@@ -110,10 +148,18 @@ function floatEquals(a: number, b: number, epsilon = 0.0001) {
 
 export const deleteMetricValueFromFile = async (dateTime: string, metric: string, value: number) => {
     try {
+
+        // File is always UTC in the file, but the incoming dateTime string is in local.  Need to convert to UTC
+        const dateTimeUTC = new Date(dateTime);
+        // Now pull out the UTC values
+        const dateTimeUTCDateString = dateTimeUTC.toISOString().split('T')[0]
+        const dateTimeUTCTimeString = dateTimeUTC.toISOString().split('T')[1].slice(0, 5)
+        const dateTimeUTCString = `${dateTimeUTCDateString} ${dateTimeUTCTimeString}`;
+
         console.log("Deleting metric value from file with metric, dateTime, value:", dateTime, metric, value);
-        const entries = await readMetricValuesFromFile();
+        const entries = await readMetricValuesFromFile(false);
         const updatedEntries = entries.filter((entry: { dateTime: string, metric: string, value: number }) =>
-            !(entry.dateTime === dateTime && entry.metric === metric && floatEquals(entry.value, value))
+            !(entry.dateTime === dateTimeUTCString && entry.metric === metric && floatEquals(entry.value, value))
         );
 
         const fileContent = updatedEntries.map(entry => `${entry.dateTime},${entry.metric},${entry.value}`).join('\n');
